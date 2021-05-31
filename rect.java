f46c228510b95f215c27678ee9ae77a853a5a1b1
Nayana2898 <64266639+Nayana2898@users.noreply.github.com>
2021-05-31 00:55:40 -0700

    Update rect.java

--- rect.java
+++ rect.java
@@ -15,24 +15,24 @@ class rect
 	{
 	    for(int j=0; j<c; j++)
 	     {
-	         if (i == 0 && j == 0 || i == 0 && j == c-1 || i == r-1 && j == 0 || i == r-1 && j == c-1)
-                              System.out.print("+");
+	        if (i == 0 && j == 0 || i == 0 && j == c-1 || i == r-1 && j == 0 || i == r-1 && j == c-1)
+                    System.out.print("+");
 	        else if (i == 0 || i == r-1)
 	            System.out.print("-");
-                          else if (j == 0 || j == c-1)
+                else if (j == 0 || j == c-1)
 	            System.out.print("|");
-                          else if (i == r-3 && j == c - (a+2))
+                else if (i == r-3 && j == c - (a+2))
 	          {
 	             System.out.print(name);
 	             j += a-1;
 	          }
 	        else
-                           {
+                 {
 	            System.out.print(" ");
 	         }
 	      }
 	System.out.println( );
-                  }
+         }
    }
 }
 	
